642a9108671d1026207fbe7f925aa1493d2138de
SergeySuhatsky <88921698+SergeySuhatsky@users.noreply.github.com>
2026-07-26 22:14:50 +0300

    Add auto registation pages in shell via reflexion and atribut

--- Zubrium.Maui/AppShell.xaml.cs
+++ Zubrium.Maui/AppShell.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace Zubrium.Maui
+﻿using System.Reflection;
+using Zubrium.Maui.Attributes;
+
+namespace Zubrium.Maui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoutesAutomated();
+        }
+    
+
+    private void RegisterRoutesAutomated()
+        {
+            // Получаем все классы в текущей сборке
+            var pagesWithRoute = Assembly.GetExecutingAssembly().GetTypes()
+                // Оставляем только те, что наследуются от ContentPage (не абстрактные)
+                .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(ContentPage)))
+                // Ищем только те, у которых есть наш атрибут [ShellRoute]
+                .Where(t => t.GetCustomAttribute<ShellRouteAttribute>() != null);
+
+            foreach (var pageType in pagesWithRoute)
+            {
+                // Регистрируем маршрут. pageType.Name вернет название класса (например, "ArticleDetailsPage")
+                Routing.RegisterRoute(pageType.Name, pageType);
+            }
         }
     }
 }
---
+++ Zubrium.Maui/Attributes/ShellRouteAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zubrium.Maui.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ShellRouteAttribute: Attribute
+    {
+    }
+}
