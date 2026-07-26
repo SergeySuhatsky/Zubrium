b151fd586b9019fd25b8f87f6a61c666adaae591
SergeySuhatsky <88921698+SergeySuhatsky@users.noreply.github.com>
2026-07-26 22:16:47 +0300

    Create pickQuizPage

--- Zubrium.Maui/Features/Quizs/PickQuizPage.xaml.cs
+++ Zubrium.Maui/Features/Quizs/PickQuizPage.xaml.cs
@@ -1,5 +1,8 @@
+using Zubrium.Maui.Attributes;
+
 namespace Zubrium.Maui.Features.Quizs;
 
+[ShellRoute]
 public partial class PickQuizPage : ContentPage
 {
 	public PickQuizPage()
---
+++ Zubrium.Maui/Features/Quizs/PickQuizViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zubrium.Content;
+using Zubrium.Maui.ViewModels;
+
+namespace Zubrium.Maui.Features.Quizs
+{
+    public partial class PickQuizViewModel: BaseViewModel
+    {
+
+        public PickQuizViewModel(IContentRepository repository) : base(repository)
+        {
+        }
+
+        public override void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+
+        }
+    }
+}
