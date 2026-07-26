9887deb97a3235be7d88787fdff8390223ed86e9
SergeySuhatsky <88921698+SergeySuhatsky@users.noreply.github.com>
2026-07-26 22:16:05 +0300

    Rename cardspage to studypage

---
+++ Zubrium.Maui/Features/Generals/StudyPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Zubrium.Maui.Attributes;
+using Zubrium.Maui.Services.MarkdownRender;
+
+namespace Zubrium.Maui.Features.Generals;
+
+
+public partial class StudyPage : ContentPage
+{
+    public StudyPage(StudyViewModel viewModel)
+    {
+
+        InitializeComponent();
+        BindingContext = viewModel;
+
+    }
+
+}
---
+++ Zubrium.Maui/Features/Generals/StudyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Zubrium.Content;
+using Zubrium.Maui.ViewModels;
+
+namespace Zubrium.Maui.Features.Generals
+{
+    public partial class StudyViewModel : BaseViewModel
+    {
+
+        public StudyViewModel(IContentRepository repository) : base(repository)
+        {
+        }
+
+        public override void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            
+        }
+
+        [RelayCommand]
+        public async Task TakeQuiz() 
+        {
+            await Shell.Current.GoToAsync(nameof(Quizs.PickQuizPage));
+        }
+    }
+}
