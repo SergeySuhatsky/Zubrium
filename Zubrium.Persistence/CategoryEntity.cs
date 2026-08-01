801e0564cd7eb4d93592253030dcfff540e6a501
SergeySuhatsky <88921698+SergeySuhatsky@users.noreply.github.com>
2026-08-01 20:34:11 +0300

    Create category entity

---
+++ Zubrium.Persistence/CategoryEntity.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zubrium.Persistence
+{
+    public class CategoryEntity
+    {
+        [PrimaryKey]
+        public string DbId { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
