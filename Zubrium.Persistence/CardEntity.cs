4ec68692b80889d9774c82762b13e5fb0ff16a1a
SergeySuhatsky <88921698+SergeySuhatsky@users.noreply.github.com>
2026-08-02 16:34:12 +0300

    Create db entity for card, quiz and modify article

---
+++ Zubrium.Persistence/CardEntity.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zubrium.Persistence
+{
+    [Table("Cards")]
+    public class CardEntity
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string FrontMarkdown { get; set; }
+
+        public string BriefMarkdown { get; set; }
+
+        public string? DetailedMarkdown { get; set; }
+
+        [Indexed]
+        public string CategoryId { get; set; }
+
+
+    }
+}
